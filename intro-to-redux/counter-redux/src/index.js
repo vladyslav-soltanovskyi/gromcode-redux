5145dcec3bf9818f7773912fa17f5091f6ce1f37
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-23 20:55:04 +0100

    finished task 'counter-redux'

---
+++ intro-to-redux/counter-redux/src/index.js
@@ -0,0 +1,24 @@
+import { createStore } from 'redux';
+
+const INCREMENT =  'COUNTER/INCREMEN';
+const DECREMENT =  'COUNTER/DECREMENT';
+
+export const increment = () => ({ type: INCREMENT });
+
+export const decrement = () => ({ type: DECREMENT });
+
+export const counterReducer = (state = 0, action) => {
+  switch(action.type) {
+    case INCREMENT: {
+      return state + 1
+    }
+    case DECREMENT: {
+      return state - 1;
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export const store = createStore(counterReducer);
