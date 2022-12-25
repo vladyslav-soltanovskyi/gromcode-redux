58446c8547e245e96a770b77c3db63faceae1a94
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-25 15:45:40 +0100

    finished task 'combining-reducers'

--- combining-reducers/task1-combining-reducers/src/users.actions.js
+++ combining-reducers/task1-combining-reducers/src/users.actions.js
@@ -3,5 +3,5 @@ export const DELETE_USER =  'USERS/DELETE_USER';
 export const UPDATE_USER =  'USERS/UPDATE_USER';
 
 export const addUser = (dataUser) => ({ type: ADD_USER, payload: dataUser });
-export const deleteUser = (userId) => ({ type: DELETE_USER, payload: userId });
-export const updateUser = (userId, dataUser) => ({ type: DELETE_USER, payload: { userId, dataUser } });
+export const deleteUser = (userId) => ({ type: DELETE_USER, payload: { userId } });
+export const updateUser = (userId, dataUser) => ({ type: UPDATE_USER, payload: { userId, dataUser } });
--- combining-reducers/task1-combining-reducers/src/users.reducer.js
+++ combining-reducers/task1-combining-reducers/src/users.reducer.js
@@ -9,7 +9,7 @@ export const usersReducer = (state = initialState, action) => {
     case ADD_USER: 
       return { ...state, usersList: [...state.usersList, action.payload] };
     case DELETE_USER:
-      return { ...state, usersList: state.usersList.filter(user => user.id !== action.payload) };
+      return { ...state, usersList: state.usersList.filter(user => user.id !== action.payload.userId) };
     case UPDATE_USER: {
       const { userId, dataUser } = action.payload;
       return { ...state, usersList: state.usersList.map(user => user.id !== userId ? user : { ...user, ...dataUser }) };
