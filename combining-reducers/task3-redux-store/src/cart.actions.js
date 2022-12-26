0534d8c1e993f9c134b096ab2ab6ced06d21692c
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-26 18:22:10 +0100

    finished task 'redux-store'

--- combining-reducers/task3-redux-store/src/cart.actions.js
+++ combining-reducers/task3-redux-store/src/cart.actions.js
@@ -2,4 +2,4 @@ export const ADD_PRODUCT =  'CART/ADD_PRODUCT';
 export const REMOVE_PRODUCT =  'CART/REMOVE_PRODUCT';
 
 export const addProduct = (dataProduct) => ({ type: ADD_PRODUCT, payload: { dataProduct } });
-export const removeProduct = (productId) => ({ type: DELETE_PRODUCT, payload: { productId } });
+export const removeProduct = (productId) => ({ type: REMOVE_PRODUCT, payload: { productId } });
