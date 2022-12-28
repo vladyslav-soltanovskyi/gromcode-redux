554133638890f6d3b18a3e15090e87d5f43c3cc2
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-28 19:30:55 +0100

    finished task 'pagination-react-redux'

--- react-with-redux/pagination-react-redux/src/users/Pagination.jsx
+++ react-with-redux/pagination-react-redux/src/users/Pagination.jsx
@@ -12,7 +12,7 @@ function Pagination({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) {
       onClick={goPrev}
       disabled={isPrevPageAvailable}
     >
-      {!isPrevPageAvailable && '←'}
+      {!isPrevPageAvailable ? '←' : ''}
     </button>
     <span className="pagination__page">{currentPage + 1}</span>
     <button
@@ -20,7 +20,7 @@ function Pagination({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) {
       onClick={goNext}
       disabled={isNextPageAvailable}
     >
-      {!isNextPageAvailable && '→'}
+      {!isNextPageAvailable ? '→' : ''}
     </button>
   </div>
   );
